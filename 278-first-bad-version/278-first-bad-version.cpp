50efd03fcf7e44a37a6db59b4eedfabf7bdda18a
PIKACHU-7892 <97588897+PIKACHU-7892@users.noreply.github.com>
2022-06-16 15:52:42 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (67.75%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -0,0 +1,28 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        long long int low =1 ;
+        long long int high = n ;
+        long long int mid ; 
+        while(low<=high)
+        {
+            long long mid = (low + high)/2;
+            if(!isBadVersion(mid) )
+            {
+                low = mid +1 ;
+            }
+            if(isBadVersion(mid) && !isBadVersion(mid-1))
+            {
+                return mid;
+            }
+            if(isBadVersion(mid))
+            {
+                high = mid -1 ;
+            }
+        }
+       return n;
+    }
+};
