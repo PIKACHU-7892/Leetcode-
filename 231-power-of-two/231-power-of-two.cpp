f046e5841eb64ef15827e0516c3129191123ac2c
PIKACHU-7892 <97588897+PIKACHU-7892@users.noreply.github.com>
2022-06-28 18:05:20 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (70.04%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0)
+        {
+            return false;
+        }
+        if(log2(n)==(int)log2(n))
+        {
+            return true;
+            
+        }
+        else
+        {
+return false ;}
+    }
+};
