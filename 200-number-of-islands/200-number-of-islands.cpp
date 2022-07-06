24967600c28554b2bab1a06e76e4b94d2b5636f0
PIKACHU-7892 <97588897+PIKACHU-7892@users.noreply.github.com>
2022-07-06 16:20:39 +0530

    Time: 69 ms (26.73%), Space: 12.1 MB (97.23%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,29 +1,33 @@
 class Solution {
-
 public:
-        public:
+    void erase(vector<vector<char>>& grid,int i , int j,int m , int n )
+    {
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]=='0')
+        {
+            return ; 
+        }
+        grid[i][j]='0';
+        erase(grid,i,j+1,m,n);
+        erase(grid,i,j-1,m,n);
+        erase(grid,i+1,j,m,n);
+        erase(grid,i-1,j,m,n);
+        
+    }
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size(), n = m ? grid[0].size() : 0, islands = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
-                    islands++;
-                    eraseIslands(grid, i, j);
+        int m = grid.size();
+        int n = grid[0].size();
+        int count =0 ; 
+        for(int i = 0 ; i<m ;i++)
+        {
+            for(int j = 0 ; j<n;j++)
+            {
+                if(grid[i][j]=='1')
+                {
+count++;
+                    erase(grid,i,j,m,n);
                 }
             }
         }
-        return islands;
-    }
-private:
-    void eraseIslands(vector<vector<char>>& grid, int i, int j) {
-        int m = grid.size(), n = grid[0].size();
-        if (i < 0 || i == m || j < 0 || j == n || grid[i][j] == '0') {
-            return;
-        }
-        grid[i][j] = '0';
-        eraseIslands(grid, i - 1, j);
-        eraseIslands(grid, i + 1, j);
-        eraseIslands(grid, i, j - 1);
-        eraseIslands(grid, i, j + 1);
+        return count ;
     }
 };
