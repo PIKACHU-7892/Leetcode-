e75978f6d77eae731cb10d1852eb113a04bd223a
PIKACHU-7892 <97588897+PIKACHU-7892@users.noreply.github.com>
2022-07-15 11:14:42 +0530

    Time: 3 ms (70.64%), Space: 9.6 MB (80.35%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.cpp
+++ 226-invert-binary-tree/226-invert-binary-tree.cpp
@@ -17,11 +17,12 @@ public:
             
             return NULL;
         }
+        invertTree(root->left);
+        invertTree(root->right);
         TreeNode* temp = root->left ;
         root->left = root->right; 
         root->right = temp;
-        invertTree(root->left);
-        invertTree(root->right);
+        
         return root;
     }
 };
