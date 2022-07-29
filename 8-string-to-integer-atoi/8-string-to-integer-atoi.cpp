b5b1d9053ae2cce7ec7201a9f193a15d137ce0dd
PIKACHU-7892 <97588897+PIKACHU-7892@users.noreply.github.com>
2022-07-30 00:04:36 +0530

    Time: 10 ms (17.61%), Space: 7.1 MB (52.96%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int sign = 1;
+        int result ;
+        int value  = 0;
+        int flag =1  ;
+        int flag2 = 1 ;
+        for(int i = 0 ; i<s.size();i++)
+        {
+            if(s[i]==' ' && flag2)
+            {
+                if(!flag)
+                {
+                    break;
+                }
+                continue;
+            }
+            if(s[i]=='+' && flag)
+            {
+                sign = 1;
+                flag =0;
+                continue ;
+            }
+            if(s[i]=='-' && flag)
+            {
+                sign = -1 ;
+                flag = 0;
+                continue ;
+            }
+            if(isdigit(s[i]))
+            {
+                flag =0;
+                flag2 = 0;
+            }
+         if(!isdigit(s[i]))
+             {
+                 break; 
+             //2147483647
+            }
+            if((value>(INT_MAX/10) || ((value==(INT_MAX/10)&& (s[i]-'0')>7))))
+            {
+                
+                if(sign==1)
+                {
+                    return INT_MAX;
+                }
+                else
+                {
+                    return INT_MIN;
+                }
+            }
+            
+            value = value*10 + (s[i]-'0');
+        }
+        return sign*value ;
+    }
+};
